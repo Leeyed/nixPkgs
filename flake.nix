524898edf6863a099a22b0b3303738c67ba1cb88
leeyed <ls0315@163.com>
2022-06-28 18:33:25 +0800

    add lowdosepet

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs.SimpleITKPkgs.url = "git+ssh://git@github.com/lizi002/nix-SimpleITK?ref=main";
   inputs.torchioPkgs.url = "./torchio";
   inputs.monaiPkgs.url = "./monai";
+  inputs.lowdosepetPkgs.url = "git+ssh://git@github.com/chengaoyu/LowDosePET.git?ref=main";
+
 
   outputs = { self, nixpkgs, flake-utils, SimpleITKPkgs, torchioPkgs, monaiPkgs }:
     (flake-utils.lib.eachDefaultSystem (system:
@@ -15,6 +17,7 @@
           SimpleITK = SimpleITKPkgs.packages.${system}.SimpleITK;
           torchio = torchioPkgs.packages.${system}.torchio;
           monai = monaiPkgs.packages.${system}.monai;
+          lowdosepet = lowdosepetPkgs.packages.${system}.lowdosepet;
         };
 
 
